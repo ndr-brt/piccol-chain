0e1857ce2addb4920a268cb1bffe7e142cd2b0a4
Andrea Bertagnolli <andrea.bertagnolli@gmail.com>
2018-02-27 17:27:29 +0100

    Improve main

--- src/main/java/srl/paros/piccolchain/Main.java
+++ src/main/java/srl/paros/piccolchain/Main.java
@@ -9,7 +9,8 @@ import srl.paros.piccolchain.node.Node;
 
 import java.util.Random;
 import java.util.UUID;
-import java.util.stream.IntStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
@@ -23,32 +24,50 @@ public class Main {
 
         log.info("Guest list created. Id {}", guestList.id());
 
-        UUID nodeId = UUID.fromString(Unirest.post(BASE_URL + guestList.id() + "/").asString().getBody());
-        Node firstNode = new Node(nodeId);
-        firstNode.init();
+        int nodes = 2;
 
-        log.info("Registered new node {}", firstNode.id());
+        Executors.newFixedThreadPool(nodes).submit(() -> {
+            Node node = new Node(createNode(guestList));
+            node.init();
+
+            log.info("Registered new node {}", node.id());
+
+            while (true) {
+
+                for (int i = 0; i < random(5); i++) {
+                    newTransaction(node);
+                }
+
+                log.info("Mine a block {}", mine(node));
+
+                Thread.sleep(random(1000));
+
+                log.info("Blockchain {}", blockChainOf(node));
+            }
+
+        });
+
+        Thread.sleep(2000);
+
         log.info("All nodes {}", allNodes(guestList));
-        log.info("Blockchain at start {}", blockChainOf(firstNode));
-
-        for (int i = 0; i < random(5); i++) {
-            newTransaction(firstNode);
-        }
-
-        log.info("Mine a block {}", mine(firstNode));
-
-        for (int i = 0; i < random(5); i++) {
-            newTransaction(firstNode);
-        }
-
-        log.info("Mine a block {}", mine(firstNode));
-
-        log.info("Blockchain {}", blockChainOf(firstNode));
-
     }
 
-    private static String mine(Node node) throws UnirestException {
-        return Unirest.get(BASE_URL + node.id() + "/mine").asString().getBody();
+    private static UUID createNode(GuestList guestList) {
+        try {
+            return UUID.fromString(Unirest.post(BASE_URL + guestList.id() + "/")
+                    .asString()
+                    .getBody());
+        } catch (UnirestException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String mine(Node node) {
+        try {
+            return Unirest.get(BASE_URL + node.id() + "/mine").asString().getBody();
+        } catch (UnirestException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static String newTransaction(Node node) {
@@ -62,12 +81,24 @@ public class Main {
         }
     }
 
-    private static String blockChainOf(Node node) throws UnirestException {
-        return Unirest.get(BASE_URL + node.id() + "/blocks").asString().getBody();
+    private static String blockChainOf(Node node) {
+        try {
+            return Unirest.get(BASE_URL + node.id() + "/blocks")
+                    .asString()
+                    .getBody();
+        } catch (UnirestException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private static String allNodes(GuestList guestList) throws UnirestException {
-        return Unirest.get(BASE_URL + guestList.id() + "/").asString().getBody();
+    private static String allNodes(GuestList guestList) {
+        try {
+            return Unirest.get(BASE_URL + guestList.id() + "/")
+                    .asString()
+                    .getBody();
+        } catch (UnirestException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static Transaction randomTransaction() {
